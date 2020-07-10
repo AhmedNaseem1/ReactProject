df499a74d5998f8241cd2f2725c11aa748f012c8
AhmedNaseem1 <an1409647@gmail.com>
2020-07-10 07:03:39 +0200

    Updates

---
+++ precache-manifest.b3158462046109fc2431e692552445f2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1ccb1830e9c2fa67866260368ce79a0",
+    "url": "/ReactProject/index.html"
+  },
+  {
+    "revision": "6fd0f95a9d70152bc958",
+    "url": "/ReactProject/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "dbb21e0e3fdb08179417",
+    "url": "/ReactProject/static/css/main.3c7467af.chunk.css"
+  },
+  {
+    "revision": "6fd0f95a9d70152bc958",
+    "url": "/ReactProject/static/js/2.11f72476.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactProject/static/js/2.11f72476.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbb21e0e3fdb08179417",
+    "url": "/ReactProject/static/js/main.476ff64c.chunk.js"
+  },
+  {
+    "revision": "15beff8eed8bf80c23a9",
+    "url": "/ReactProject/static/js/runtime-main.586df27e.js"
+  },
+  {
+    "revision": "54c25add05dff22a42a8b8d874ecbc3e",
+    "url": "/ReactProject/static/media/kaaba.54c25add.jpeg"
+  }
+]);
